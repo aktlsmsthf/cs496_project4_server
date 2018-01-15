097a71d3b4ea12c93bf13c40dc5b71d42fb44aff
aktlsmthf <lch7224@kaist.ac.kr>
2018-01-16 01:21:25 +0900

    5

--- catalog/views.py
+++ catalog/views.py
@@ -98,6 +98,8 @@ def data_return(request):
 		data = img.reshape([1, 784])
 		data = 1 - (data/255)
 
+		scipy.misc.imsave('image/test.png', img)
+
 		return render(request, 'test.html',{})
 
 		'''
