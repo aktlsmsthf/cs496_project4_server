cc46911854db163c9384e709629afc0d68e1fc6d
algogae <kihs0468@kaist.ac.kr>
2018-01-17 18:56:22 +0830

    Applying fancy algorithm

--- catalog/models.py
+++ catalog/models.py
@@ -88,11 +88,14 @@ class MnistNN(models.Model):
 
 		#saver.restore(sess, save_path)
 
+		# TEST
 		if(not train):
 			saver.restore(sess, save_path)
 			prediction = sess.run(tf.nn.softmax(logits, 1), feed_dict={X: data, keep_prob:1})
 			p = prediction[0]
 			print(p)
+
+		# TRAINING
 		else:
 			total_data = 110
 			batch_x, batch_y = mnist.train.next_batch(total_data)
@@ -101,20 +104,27 @@ class MnistNN(models.Model):
 				path = 'image/'+str(image.path)+'.jpg'
 				im = Image.open(path)
 				img = array(im.resize((28,28), Image.ANTIALIAS).convert("L"))
-				data = img.reshape([1, 784])
-				data = 1-(data/255)
-				batch_x = np.concatenate((batch_x, data))
-				label = [0,0,0,0,0,0,0,0,0,0]
-				label[image.label]=1
-				batch_y = np.concatenate((batch_y, [label]))
-				total_data+=1
+
+				# DATA_AUGMENTATION_ALGORITHM
+				list_of_img_arr = []
+				blur(img_arr, list_of_img_arr)
+				shift(img_arr, list_of_img_arr)
+
+				for data in list_of_img_arr
+					data = img.reshape([1, 784])
+					data = 1-(data/255)
+					batch_x = np.concatenate((batch_x, data))
+					label = [0,0,0,0,0,0,0,0,0,0]
+					label[image.label]=1
+					batch_y = np.concatenate((batch_y, [label]))
+					total_data+=1
 
 
 			total_batch = int(total_data/batch_size)
-			
+
 			for epoch in range(10):
 				total_cost = 0
-				for i in range(total_batch):	
+				for i in range(total_batch):
 					batch_xs, batch_ys = batch_x[i*batch_size:(i+1)*batch_size], batch_y[i*batch_size:(i+1)*batch_size]
 					_, cost_val = sess.run([optimizer, cost], feed_dict = {X:batch_xs, Y:batch_ys, keep_prob:0.8})
 				total_cost+=cost_val
@@ -126,11 +136,133 @@ class MnistNN(models.Model):
 			print('Accuracy:', sess.run(accuracy, feed_dict={X: mnist.test.images, Y:mnist.test.labels, keep_prob:1}))
 			self.accuracy = sess.run(accuracy, feed_dict={X: mnist.test.images, Y:mnist.test.labels, keep_prob:1})*100
 			self.save()
-			
+
+	# Blurring effect
+	def blur(img_arr, list_of_img_arr):
+		img_arr = 1 - img_arr/255
+		noise = np.zeros((28, 28))
+
+		# examine all entries
+		for i in range(28):
+		    for j in range(28):
+		        temp = img_arr.item((i, j))
+		        # first_row
+		        if (i == 0):
+		            # detect nonzero entry
+		            if (temp > 0):
+		                # add noise
+		                noise[i+1,j] += temp*np.random.random()
+		                if (j > 0):
+		                    noise[i,j-1] += temp*np.random.random()
+		                    noise[i+1,j-1] += temp*np.random.random()
+		                if (j < 27):
+		                    noise[i,j+1] += temp*np.random.random()
+		                    noise[i+1,j+1] += temp*np.random.random()
+		        # last_row
+		        elif (i == 27):
+		            # detect nonzero entry
+		            if (temp > 0):
+		                # add noise
+		                noise[i-1,j] += temp*np.random.random()
+		                if (j > 0):
+		                    noise[i,j-1] += temp*np.random.random()
+		                    noise[i-1,j-1] += temp*np.random.random()
+		                if (j < 27):
+		                    noise[i,j+1] += temp*np.random.random()
+		                    noise[i-1,j+1] += temp*np.random.random()
+		        # others
+		        else:
+		            # detect nonzero entry
+		            if (temp > 0):
+		                # add noise
+		                noise[i-1,j] += temp*np.random.random()
+		                noise[i+1,j] += temp*np.random.random()
+		                if (j > 0):
+		                    noise[i-1,j-1] += temp*np.random.random()
+		                    noise[i,j-1] += temp*np.random.random()
+		                    noise[i+1,j-1] += temp*np.random.random()
+		                if (j < 27):
+		                    noise[i-1,j+1] += temp*np.random.random()
+		                    noise[i,j+1] += temp*np.random.random()
+		                    noise[i+1,j+1] += temp*np.random.random()
+
+		img_noise = img_arr + noise
+
+		# semi-Calibration
+		for i in range(28):
+		    for j in range(28):
+		        if (img_noise.item((i,j)) > 1):
+		            img_noise[i,j] = 1
+
+		list_of_img_arr.append(img_noise)
+		return
+
+	# Shifting effect
+	def shift(img_arr, list_of_img_arr):
+		img_arr = 1 - img_arr/255
+
+		# Threshold
+		threshold = 0.5
+
+		# REPRODUCE_BY_SHIFTING_LEFT
+		shift_dist = 27
+		for i in range(28):
+		    for j in range(28):
+		        if (threshold < img_arr.item((i, j)) and j < shift_dist):
+		            shift_dist = j
+		            break
+
+		for shift in range(shift_dist):
+		    img_shift = np.zeros((28, 28))
+		    img_shift[:,:28-shift] = img_arr[:,shift:]
+		    list_of_img_arr.append(img_shift)
+
+		# REPRODUCE_BY_SHIFTING_RIGHT
+		shift_dist = 0
+		for i in range(28):
+		    for j in reversed(range(28)):
+		        if (threshold < img_arr.item((i, j)) and shift_dist < j):
+		            shift_dist = j
+		            break
+
+		for shift in range(28-shift_dist):
+		    img_shift = np.zeros((28, 28))
+		    img_shift[:, shift:shift_dist+shift] = img_arr[:, :shift_dist]
+		    list_of_img_arr.append(img_shift)
+
+
+		# REPRODUCE_BY_SHIFTING_ABOVE
+		img_arr_trans = img_arr.transpose()
+		shift_dist = 27
+		for i in range(28):
+		    for j in range(28):
+		        if (threshold < img_arr_trans.item((i, j)) and j < shift_dist):
+		            shift_dist = j
+		            break
+
+		for shift in range(shift_dist):
+		    img_shift = np.zeros((28, 28))
+		    img_shift[:28-shift,:] = img_arr[shift:,:]
+		    list_of_img_arr.append(img_shift)
+
+		# REPRODUCE_BY_SHIFTING_BELOW
+		shift_dist = 0
+		for i in range(28):
+		    for j in reversed(range(28)):
+		        if (threshold < img_arr_trans.item((i, j)) and shift_dist < j):
+		            shift_dist = j
+		            break
+
+		for shift in range(28-shift_dist):
+		    img_shift = np.zeros((28, 28))
+		    img_shift[shift:shift_dist+shift,:] = img_arr[:shift_dist,:]
+		    list_of_img_arr.append(img_shift)
+		return
+
+
+
 class TestData(models.Model):
 	pred = models.IntegerField()
 
 	def __str__(self):
 		return self.ped
-
-
